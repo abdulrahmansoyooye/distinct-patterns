629354a071adf287e9b870f8c0254f6db5863423
zubayr <abdussalamazzubayr@gmail.com>
2024-12-06 20:54:23 +0100

    feat(app): not showing yet Signin using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import "../styles/globals.css";
 import Navbar from "../components/Navbar.jsx";
 import { Poppins } from "next/font/google";
 import { StateContext } from "../context/StateContext.js";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 const poppins_font = Poppins({
   weight: "400",
   subsets: ["latin"],
@@ -10,15 +17,27 @@ export const metadata = {
   title: "Distinct Patterns",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <StateContext>
         <body className={poppins_font.className}>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           <Navbar />
           {children}
         </body>
       </StateContext>
     </html>
+    </ClerkProvider>
   );
 }
